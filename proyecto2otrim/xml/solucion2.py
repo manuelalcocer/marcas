030320908f79443db354348576b38cd311480447
Manuel Alcocer <m.alcocer1978@gmail.com>
2016-03-07 20:10:37 +0100

    Lista de ciudades

--- proyecto2otrim/xml/solucion2.py
+++ proyecto2otrim/xml/solucion2.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python2
+# -*- coding: utf-8 -*- 
 
 ## 2. Contar las ciudades de cada país, y mostrar la mas septentrional y meridional
 
+from lxml import etree
 
+raiz = etree.parse('./mondial-3.0.xml').getroot()
+
+for pais in raiz.iter('country'):
+    for ciudad in pais.iter('city'):
+        print ciudad.find('name').text.strip()
