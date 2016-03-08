8b491151094f508831f128a080f3372f73d57dd4
Manuel Alcocer <m.alcocer1978@gmail.com>
2016-03-08 08:42:14 +0100

    finalizado. muy corto 1  commit

--- proyecto2otrim/json/solucion1.py
+++ proyecto2otrim/json/solucion1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-# 1. Listar de los sites de opendata:
+# 1. Lista de los sites de opendata:
 #	- Nombre
 #	- Autor
 #	- País
@@ -10,3 +10,11 @@
 from json import loads
 with open('./opendata.json') as archivo:
     datos = archivo.read()
+
+datos_dict = loads(datos)
+
+for clave in datos_dict.keys():
+    print '\n\nNombre: ', datos_dict[clave]['name']
+    print '\tAutor: ', datos_dict[clave]['author']
+    print '\tPaís: ', datos_dict[clave]['country']
+    print '\tDescripción:', datos_dict[clave]['description']
