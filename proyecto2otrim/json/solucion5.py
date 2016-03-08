66301547631684f164ec98c4be24f9405c61f498
Manuel Alcocer <m.alcocer1978@gmail.com>
2016-03-08 18:46:15 +0100

    inicio del cálculo del área

--- proyecto2otrim/json/solucion5.py
+++ proyecto2otrim/json/solucion5.py
@@ -19,22 +19,45 @@ for clave in datos_dict.keys():
     if datos_dict[clave]['country'].upper() not in paises and 0 < len(datos_dict[clave]['country']) < 3:
         paises[datos_dict[clave]['country'].upper()] = []
     if 0 < len(datos_dict[clave]['country']) < 3 and len(datos_dict[clave]['location']) > 2:
-        print 'lllll'
         paises[datos_dict[clave]['country'].upper()].append([datos_dict[clave]['country'].upper(), datos_dict[clave]['location'],datos_dict[clave]['url']])
 
+# Para el ejercicio solo tiene en cuentan los paises que tienen al menos 4 sites de opendata en la lista
 print '\nListado de paises disponibles:\n'
 for clave in paises.keys():
-    print clave, paises[clave],
+    if len(paises[clave]) < 4:
+        paises.pop(clave)
+        continue
+    print clave,
 
 correcto = False
 while not correcto:
-    paiselegido = raw_input('\nElige un país de la lista:')
-    if paiselegido.upper() in paises:
+    paiselegido = raw_input('\nElige un país de la lista:').upper()
+    if paiselegido in paises:
         correcto = True
 
 # p0 = min,min
 # p1 = min,max
 # p2 = max,min
 # p3 = max,max
+if len(paises[paiselegido]) == 3:
+    p0 = float(paises[paiselegido][1].split(','))
+    p0 = [p0[1],p0[0]]
+    p1 = float(paises[paiselegido][1].split(','))
+    p1 = [p1[1],p1[0]]
+    p2 = float(paises[paiselegido][1].split(','))
+    p2 = [p2[1],p2[0]]
+    for site in paises[paiselegido]:
+        if float(paises[paiselegido][1].split(',')[1]) < p0[0] and float(paises[paiselegido][1].split(',')[0]) < p0[1]:
+            p0 = [float(paises[paiselegido][1].split(',')[1]),float(paises[paiselegido][1].split(',')[0]) < p0[1]]
+        elif float(paises[paiselegido][1].split(',')[1]) < p0[0] and float(paises[paiselegido][1].split(',')[0]) < p0[1]:
+            p0 = [float(paises[paiselegido][1].split(',')[1]),float(paises[paiselegido][1].split(',')[0]) < p0[1]]
 
-
+            pass
+p0 = float(paises[paiselegido][1].split(','))
+p0 = [p0[1],p0[0]]
+p1 = paises[paiselegido][1].split(',')
+p0 = [p0[1],p0[0]]
+p2 = paises[paiselegido][1].split(',')
+p0 = [p0[1],p0[0]]
+p3 = paises[paiselegido][1].split(',')
+p0 = [p0[1],p0[0]]
