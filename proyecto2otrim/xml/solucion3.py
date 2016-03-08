7a12e8a01d276bae83cd6ca17557adab281fa57e
Manuel Alcocer <m.alcocer1978@gmail.com>
2016-03-08 06:26:28 +0100

    Finalizado

--- proyecto2otrim/xml/solucion3.py
+++ proyecto2otrim/xml/solucion3.py
@@ -9,7 +9,7 @@ raiz = etree.parse('./mondial-3.0.xml').getroot()
 
 for pais in raiz.iter('country'):
     try:
-        densidad = round(float(pais.attrib['population'])/float(pais.attrib['total_area']),2)
+        densidad = float(pais.attrib['population'])/float(pais.attrib['total_area'])
         try:
             if densidad > densidadmaxima:
                 densidadmaxima = densidad
@@ -24,10 +24,23 @@ for pais in raiz.iter('country'):
         # si no hay datos para calcular la densidad no se hace nada
         pass
 
-print 'Densidad maxima: %f' % densidadmaxima
-print 'Densidad mínima: %f' % densidadminima
-densidad = float(raw_input('Dime una densidad comprendida entre esos valores: '))
-while densidadminima < densidad < densidadmaxima:
+print 'Densidad maxima: %.2f hab/km²' % densidadmaxima
+print 'Densidad mínima: %.2f hab/km²' % densidadminima
+densidadpedida = float(raw_input('Dime una densidad comprendida entre esos valores: '))
+while not densidadminima < densidadpedida < densidadmaxima:
     densidad = float(raw_input('Dime una densidad comprendida entre esos valores: '))
 
-
+for pais in raiz.iter('country'):
+    try:
+        densidad = round(float(pais.attrib['population'])/float(pais.attrib['total_area']),2)
+    except:
+        pass
+    if densidadpedida < densidad:
+        print '%.2f Menor: %s tiene %.2f hab/km²' %(densidadpedida,pais.attrib['name'],densidad)
+for pais in raiz.iter('country'):
+    try:
+        densidad = round(float(pais.attrib['population'])/float(pais.attrib['total_area']),2)
+    except:
+        pass
+    if densidadpedida > densidad:
+        print '%.2f Mayor: %s tiene %.2f hab/km²' %(densidadpedida,pais.attrib['name'],densidad)
