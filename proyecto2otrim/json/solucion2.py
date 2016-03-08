e41f7f8929a1e00715a755efee3797282f0129eb
Manuel Alcocer <m.alcocer1978@gmail.com>
2016-03-08 08:48:26 +0100

    Creacion del ranking

--- proyecto2otrim/json/solucion2.py
+++ proyecto2otrim/json/solucion2.py
@@ -4,10 +4,17 @@
 # 2. Contar los sites de opendata de un país cualquiera, por ejemplo, Reino Unido, crear un ranking, y decir en qué posición se encuentra.
 
 from json import loads
+
 with open('./opendata.json') as archivo:
     datos = archivo.read()
 
 datos_dict = loads(datos)
 
 contadores = {}
-
+for clave in datos_dict.keys():
+    if datos_dict[clave]['country'].upper() not in contadores:
+        contadores[datos_dict[clave]['country'].upper()] = 1
+    else:
+        contadores[datos_dict[clave]['country'].upper()] += 1
+print contadores
+        
