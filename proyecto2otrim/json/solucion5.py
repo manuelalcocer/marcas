33b36a81e5a437f1ef543a1647fc0c0b0a15cc17
Manuel Alcocer <m.alcocer1978@gmail.com>
2016-03-08 16:47:26 +0100

    inicio

---
+++ proyecto2otrim/json/solucion5.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+# 5. Pedir un país medir el área máxima que abarcan sus sites opendata y la densidad de sites que alberga.
+'''
+    Radio terrestre = Rt = 6371 km
+    grados a radianes => radianes = grados * pi / 180
+    cuerda de un circulo => l = rad * radio
+    Suponiendo esférica la tierra: 1 grado (latitud o longitud ) = pi * Rt / 180 ~ 111 km
+'''
+
+
