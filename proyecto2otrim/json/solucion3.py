7708d556ac3de282bd5522e2dcb9e7e5ac79f718
Manuel Alcocer <m.alcocer1978@gmail.com>
2016-03-08 10:59:54 +0100

    cálculo de máximo y mínimo

--- proyecto2otrim/json/solucion3.py
+++ proyecto2otrim/json/solucion3.py
@@ -4,9 +4,13 @@
 # 3. Pedir un país, mostrar la mínima y máxima fecha de ingreso en la lista opendata (metadatacreated), pedir un intervalo válido, y mostrar los sites que cumplan con el requisito
 
 from json import loads
+from datetime import datetime
 
 with open('./opendata.json') as archivo:
     datos = archivo.read()
 
 datos_dict = loads(datos)
+
+# formato de la fecha -->> "metadatacreated" : "2013-08-29T08:12:05.637Z"
+
 
